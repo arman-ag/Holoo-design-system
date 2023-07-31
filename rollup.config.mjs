9155a8b1401fd3d5d049852f4d3a07fedecdfdf1
Arman AliGHanbari <A.Alighanbari@TNC.COM>
2023-07-31 19:24:00 +0430

    first realese

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,19 +1,21 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import { babel } from '@rollup/plugin-babel';
 
 // This is required to read package.json file when
 // using Native ES modules in Node.js
 // https://rollupjs.org/command-line-interface/#importing-package-json
+import typescript from '@rollup/plugin-typescript';
 import { createRequire } from 'node:module';
+import postcss from "rollup-plugin-postcss";
+
 const requireFile = createRequire(import.meta.url);
 const packageJson = requireFile('./package.json');
 
 export default [
   {
-    input: 'src/index.js',
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
@@ -32,12 +34,21 @@ export default [
       resolve({
         extensions: ['.js', '.jsx'],
       }),
+      typescript(),
+      postcss({
+        config: {
+          modules: true,
+          path: './postcss.config.js',
+        },
+        extensions: ['.css'],
+        minimize: true,
+        inject: {
+          insertAt: 'top',
+        },
+      }),
       commonjs(),
       terser(),
-      babel({
-        extensions: ['.js', '.jsx'],
-        exclude: 'node_modules/**',
-      }),
+
     ],
     external: ['react', 'react-dom', '@emotion/react', '@emotion/styled'],
   },
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { CustomButton } from './CustomButton/CustomButton';
+import { Input } from './input/Input';
+import './tailwind.css';
+export { CustomButton, Input };
+//# sourceMappingURL=index.js.map
