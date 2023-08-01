acb23d31ac3e95d66bb1d521a6e71adff7fc9a84
Arman AliGHanbari <A.Alighanbari@TNC.COM>
2023-08-01 14:06:05 +0330

    create first story

---
+++ stories/CustomButton.stories.tsx
@@ -0,0 +1,32 @@
+import { CustomButton } from '../src/CustomButton/CustomButton';
+export default {
+  title: 'CustomButton',
+  component: CustomButton,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {
+    variant: 'outline',
+    size: 'sm',
+    asChild: false,
+    children: 'button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    variant: 'secondary',
+    size: 'lg',
+  },
+};
